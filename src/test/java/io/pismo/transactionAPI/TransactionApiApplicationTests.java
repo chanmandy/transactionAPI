4842efb7032e843a00e11ad535c9d72742cfeb7f
chanmandy <chaitanya.malse@yandex.com>
2024-06-23 15:27:11 +0530

    Spring initializr

---
+++ src/test/java/io/pismo/transactionAPI/TransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pismo.transactionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
